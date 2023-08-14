6ec7dd3055982e64116196ddf406169a0df23c53
Philipp Kohn <philipp.kohn@outlook.com>
2023-08-14 12:53:05 +0200

    Update comments

--- Test_Graph_Connect_w_Certificate.ps1
+++ Test_Graph_Connect_w_Certificate.ps1
@@ -1,8 +1,15 @@
 # Add environment variables to be used by Connect-MgGraph
 $env:AZURE_CLIENT_ID = "YOUR Client ID from your Registered App in Microsoft Entra"
 $env:AZURE_TENANT_ID = "YOUR Tenant ID"
-$CertificatePublicKey = "YOUR Authentication Certificate Public Key"
-Connect-MgGraph -ClientId $env:AZURE_CLIENT_ID -TenantId $env:AZURE_TENANT_ID -CertificateThumbprint $CertificatePublicKey
+
+# Add variable with the Thumbprint of your Certificate
+$Certificate = "The Tumbprint of your Certificate"
+Connect-MgGraph -ClientId $env:AZURE_CLIENT_ID -TenantId $env:AZURE_TENANT_ID -CertificateThumbprint $Certificate
+
+# Connect to Microsoft Graph PowerShell SDK
+Connect-MgGraph -ClientId $env:AZURE_CLIENT_ID -TenantId $env:AZURE_TENANT_ID -CertificateThumbprint $Certificate
+
+# Get Organization Infos for testing purposes
 Get-MgOrganization | Select-Object DisplayName, VerifiedDomains, ID
 
 # Disconnect from Microsoft Graph
